8e218669666903c33a445254e1df06a83841727f
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-07-20 18:17:26 -0300

    criado aula 11

---
+++ exercicios/aula11/ex08.js
@@ -0,0 +1,11 @@
+var vel = 60.5
+
+//"console.log": mostra no terminal do node.js
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+//condição simples
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+
+console.log('Dirija sempre usando o cinto de segurança!')
