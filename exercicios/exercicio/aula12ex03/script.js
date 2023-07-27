0640898565a66fba9a534f56dedcfcfd97fbdd1b
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-07-27 12:26:26 -0300

    corrigido script

--- exercicios/exercicio/aula12ex03/script.js
+++ exercicios/exercicio/aula12ex03/script.js
@@ -15,19 +15,43 @@ function verificar() {
         //cria uma tag img com id foto
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-
+        
         if (fsex[0].checked) {
             genero = 'Homem'
 
             if (idade >= 0 && idade < 10) {
                 img.setAttribute('src', 'bebeM.jpg')
+
+            } else if (idade >= 10 && idade < 21) {
+                window.alert('jovem')
+
+            } else if (idade < 50) {
+                window.alert('adulto')
+
+            } else {
+                window.alert('idoso')
+
             }
 
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                window.alert('criança')
+
+            } else if (idade >= 10 && idade < 21) {
+                window.alert('jovem')
+
+            } else if (idade < 50) {
+                window.alert('adulto')
+
+            } else {
+                window.alert('idoso')
+
+            }
         }
 
         res.innerHTML = `${genero}, ${idade} anos.`
-        res.appendChild(img)
+        res.appendChild('img')
     }
 }
