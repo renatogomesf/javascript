0e584bb5b236533e0cbf4c90647b52f659d1d8f4
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-08-02 11:11:59 -0300

    criada aula 14

---
+++ exercicios/aula14/rep02.js
@@ -0,0 +1,14 @@
+/*
+var c = 1 
+
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++ //c = c + 1
+}
+*/
+
+console.log('Vai começar...')
+for (var c = 1; c <= 5; c++) {
+    console.log(`${c}`)
+}
+console.log('fim')
