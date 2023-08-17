4f6c54fc6ac819d7e486ce795c35074c4caa4e1e
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-08-17 17:25:52 -0300

    feito mais testes de javascript

--- exercicios/atividades/TESTE/script.js
+++ exercicios/atividades/TESTE/script.js
@@ -7,6 +7,36 @@ function somar() {
     let s = n1 + n2
 
     let p = document.createElement('p')
-    p.innerText = `A soma entre ${n1} e ${n2} é ${s}`
+    p.innerText = `A soma entre ${n1} e ${n2} é igual a ${s}`
+    res.appendChild(p)
+}
+
+function subtr() {
+    let n1 = Number(num1.value)
+    let n2 = Number(num2.value)
+    let s = n1 - n2
+
+    let p = document.createElement('p')
+    p.innerText = `A subtração entre ${n1} e ${n2} é igual a ${s}`
+    res.appendChild(p)
+}
+
+function mult() {
+    let n1 = Number(num1.value)
+    let n2 = Number(num2.value)
+    let s = n1 * n2
+
+    let p = document.createElement('p')
+    p.innerText = `A multiplicação entre ${n1} e ${n2} é igual a ${s}`
+    res.appendChild(p)
+}
+
+function divi() {
+    let n1 = Number(num1.value)
+    let n2 = Number(num2.value)
+    let s = n1 / n2
+
+    let p = document.createElement('p')
+    p.innerText = `A divisão entre ${n1} e ${n2} é igual a ${s}`
     res.appendChild(p)
 }
