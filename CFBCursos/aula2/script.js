5331cc7ea088c6124c64c8bca981e3f42c46ff8f
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-09-13 16:19:21 -0300

    criada aula 3 de javascript

---
+++ CFBCursos/aula2/script.js
@@ -0,0 +1,7 @@
+"use strict"
+
+let nome = "Bruno"
+
+console.log("CFB cursos")
+console.log(nome)
+console.log("Nome: " + nome)
---
+++ CFBCursos/aula3/script.js
@@ -0,0 +1,15 @@
+
+if (true){
+    var nome1 = "Renato"
+}
+console.log(nome1)
+
+
+const curso = "JavaScript"
+console.log(curso)
+
+
+if (true){
+    let nome = "Bruno"
+}
+console.log(nome)
