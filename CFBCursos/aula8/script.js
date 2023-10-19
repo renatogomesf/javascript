d2f28b31fc8189316e94c24609751baa58cf11e4
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-10-19 19:05:30 -0300

    terminada aula8

---
+++ CFBCursos/aula8/script.js
@@ -0,0 +1,79 @@
+// &: "e"
+
+let n1 = 10
+let n2 = 11
+
+let res = n1 & n2
+
+console.log(res)
+
+
+let n3 = 11
+let n4 = 14
+
+let ress = n3 & n4
+
+console.log(ress)
+console.log("-----------------------------")
+//--------------------------------------------
+
+// |: "ou"
+
+let n5 = 10
+let n6 = 11
+
+let resss = n5 | n6
+
+console.log(resss)
+
+console.log("-----------------------------")
+//--------------------------------------------
+
+// ^: "ou esclusivo"
+
+let n7 = 10
+let n8 = 11
+
+let ressss = n7 ^ n8
+
+console.log(ressss)
+
+let n9 = 13
+let n10 = 14
+
+let resssss = n9 ^ n10
+
+console.log(resssss)
+
+console.log("-----------------------------")
+//--------------------------------------------
+
+// <</>>: "esquerda e direita"
+
+//ESQUERDA
+let n11 = 10
+
+let ressssss = n11 << 1
+
+console.log(ressssss)
+
+let n12 = 25
+
+let resssssss = n12 << 1
+
+console.log(resssssss)
+
+//DIREITA
+let n13 = 30
+
+let ressssssss = n13 >> 1
+
+console.log(ressssssss)
+
+//DESLOCANDO MAIS DE UM BIT
+
+let n14 = 20
+
+let resssssssss = n14 << 2
+
+console.log(resssssssss)
