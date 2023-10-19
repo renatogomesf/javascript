f75aa6a35a9c177cc5c580dedeb7cc68f801b837
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-10-19 17:22:32 -0300

    criada aula7

---
+++ CFBCursos/aula7/script.js
@@ -0,0 +1,29 @@
+let n1,n2,n3,n4
+n1=10
+n2=5
+n3=15
+n4=2
+
+// console.log((n1>n2)&&(n1>n3))
+
+// console.log((n1>n2)||(n1>n3))
+
+// console.log(!((n1>n2)||(n1>n3)))
+
+if (n1>n2 && n3>n4) {
+    console.log("verdadeiro")
+} else {
+    console.log("falso")
+}
+
+if (n1>n2 || n3>n4) {
+    console.log("verdadeiro")
+} else {
+    console.log("falso")
+}
+
+if (!n1>n2 && n3>n4) { //usando o operador "!"
+    console.log("verdadeiro")
+} else {
+    console.log("falso")
+}
