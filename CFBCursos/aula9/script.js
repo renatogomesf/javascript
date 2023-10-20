43b6aa57878c96a0447e076c9dd17083a82c86a7
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-10-20 18:26:05 -0300

    criado aula9

---
+++ CFBCursos/aula9/script.js
@@ -0,0 +1,15 @@
+// Pós Incremento:
+let n = 10
+
+console.log(n++)
+
+console.log(n)
+
+//-------------------------------------
+console.log("------------------")
+
+
+// Pré Incremento:
+let n1 = 10
+
+console.log(++n1)
