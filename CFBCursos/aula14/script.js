3920ee90226486fc2f561c5e362c1b5a0e19da5d
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-10-23 17:34:12 -0300

    criada aula14

---
+++ CFBCursos/aula14/script.js
@@ -0,0 +1,21 @@
+let colocacao = 6
+
+switch (colocacao) {
+    case 1: 
+        console.log("Primeiro lugar")
+        break
+    case 2:
+        console.log("Segundo lugar")
+        break
+    case 3:
+        console.log("Terceiro lugar")
+        break
+    case 4: case 5: case 6:
+        console.log("Premio de participação")
+        break
+    default:
+        console.log("Não subiu ao podio")
+        break
+}
+
+console.log("Fim do programa")
