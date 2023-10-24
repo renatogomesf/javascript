50578fab2f2c2231fea0a6918da00154be32429f
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-10-24 17:18:36 -0300

    criado aula15

---
+++ CFBCursos/aula15/script.js
@@ -0,0 +1,21 @@
+console.log("Início do programa.")
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Número ${i}.`)
+}
+
+console.log("Fim do programa.")
+
+console.log("----------------------------")
+
+console.log("Início do programa.")
+
+for (let i = 0; i <= 10; i++) {
+    if (i%2==0) { // %: retorna o resto da divisão
+        console.log(`${i} é par.`)
+    } else {
+        console.log(`${i} é ímpar.`)
+    }
+}
+
+console.log("Fim do programa.")
