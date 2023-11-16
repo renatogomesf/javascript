9ec9fb6246c481a4032ba809d21264bc28ff9409
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-11-16 18:59:39 -0300

    alterações na aula 20

--- CFBCursos/aula20/script.js
+++ CFBCursos/aula20/script.js
@@ -39,8 +39,7 @@ function mudarTexto(){
     let d2 = document.getElementById("d2")
     let d3 = document.getElementById("d3")
 
-    d1.innerHTML("CFB Cursos")
-    d2.innerHTML("CFB Cursos")
-    d3.innerHTML("CFB Cursos")
+    d1.innerHTML="CFB Cursos"
+    d2.innerHTML="CFB Cursos"
+    d3.innerHTML="CFB Cursos"
 }
-
