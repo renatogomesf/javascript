189c45b189835d177c1335ba5a57565198a44499
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-11-29 16:27:31 -0300

    criada somente a pasta da aula 29

--- CFBCursos/aula28/script.js
+++ CFBCursos/aula28/script.js
@@ -1,41 +1,41 @@
     //AULA
 
-// const cursos = ['html', 'css', 'javascript', 'php', 'react']
+const cursos = ['html', 'css', 'javascript', 'php', 'react']
 
-// cursos.map((el,i)=>{
-//     console.log(`Curso: ${el} - posição do curso: ${i}`)
-// })
+cursos.map((el,i)=>{
+    console.log(`Curso: ${el} - posição do curso: ${i}`)
+})
 
-// console.log("----------------------------------------")
+console.log("----------------------------------------")
 
-// const cursos1 = ['html', 'css', 'javascript', 'php', 'react']
+const cursos1 = ['html', 'css', 'javascript', 'php', 'react']
 
-// let c = cursos1.map((el,i)=>{
-//     return "<div>" + el + "</div>" //não é um elemento "div"
-// })
+let c = cursos1.map((el,i)=>{
+    return "<div>" + el + "</div>" //não é um elemento "div"
+})
 
-// console.log(c)
+console.log(c)
 
 //--------------------------------------------------------------------------------------------
 
     //TRABALHANDO COM HTML
 
-// let el = document.getElementsByTagName("div") //"el" não é uma coleção iterável. não é array.
+let el = document.getElementsByTagName("div") //"el" não é uma coleção iterável. não é array.
 
-// el = [...el] //el recebe o espalhamento do próprio el. deixando ele iterável. tornando um array.
+el = [...el] //el recebe o espalhamento do próprio el. deixando ele iterável. tornando um array.
 
-// el.map((e,i)=>{
-//     //com o innerHTML, retorna o que está dentro da html div. sem o inner, retorna a própria div completa.
-//     console.log(e.innerHTML)
+el.map((e,i)=>{
+    //com o innerHTML, retorna o que está dentro da html div. sem o inner, retorna a própria div completa.
+    console.log(e.innerHTML)
 
-//     //muda o que está dentro do elemento selecionado "e" pelo que está em áspas "cfb cursos"
-//     e.innerHTML = "cfb cursos"
-// })
+    //muda o que está dentro do elemento selecionado "e" pelo que está em áspas "cfb cursos"
+    e.innerHTML = "cfb cursos"
+})
 
 
-// const el = document.getElementsByTagName("div")
-// const val  = Array.prototype.map.call(el,({innerHTML})=>innerHTML)
-// console.log(val)
+const el1 = document.getElementsByTagName("div")
+const val  = Array.prototype.map.call(el1,({innerHTML})=>innerHTML)
+console.log(val)
 
 //------------------------------------------------------------------------------------
 
