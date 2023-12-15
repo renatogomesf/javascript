3b3c5082c43a3fc767d0e1361b79f481ea779630
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-12-14 21:51:49 -0300

    feita aula 45

--- CFBCursos/aulas43, 44 e 45/aula43.js
+++ CFBCursos/aulas43, 44 e 45/aula43.js
@@ -1,27 +1,41 @@
 const caixaCursos = document.querySelector("#caixaCursos")
 const btn_c = [...document.querySelectorAll(".curso")]
 const c1_2 = document.querySelector("#c1_2")
+
 const cursos = ["HTML","CSS","Javascript","PHP","React","MySQL","ReactNative"]
+
 const btnCursoSelecionado = document.getElementById("btnCursoSelecionado")
 const btnRemoverCurso = document.getElementById("btnRemoverCurso")
+const btnAdicionarAntes = document.getElementById("btnAdicionarAntes")
+const btnAdicionarDepois = document.getElementById("btnAdicionarDepois")
 
-cursos.map((el,i)=>{
+const nomeCurso = document.getElementById("nomeCurso")
+
+let indice = 0
+
+const criarNovoCurso = (curso)=>{
     const newElement = document.createElement("div")
-    newElement.setAttribute("id","c" + (i + 1))
+    newElement.setAttribute("id","c" + (indice + 1))
     newElement.setAttribute("class","curso c1")
-    newElement.innerHTML = el
-
+    newElement.innerHTML = curso
+    
     const comandos = document.createElement("div")
     comandos.setAttribute("class","comandos")
     
     const rb = document.createElement("input")
     rb.setAttribute("type","radio")
     rb.setAttribute("name","rb_cursos")
-
+    
     comandos.appendChild(rb)
     newElement.appendChild(comandos)
-    caixaCursos.appendChild(newElement)
+    
+    return newElement
+}
 
+cursos.map((el)=>{
+    const newElement = criarNovoCurso(el)
+    caixaCursos.appendChild(newElement)
+    indice++
 })
 
 const radioSelecionado = ()=>{
@@ -31,14 +45,14 @@ const radioSelecionado = ()=>{
     const radioSelecionado = todosRadios.filter((ele)=>{
         return ele.checked
     })
-
+    
     //aqui eu deixo de trabalhar com todo um array e retorno somente um único elemento da posição 0... a tag input.
     return radioSelecionado[0]
 }
 
 btnCursoSelecionado.addEventListener("click",()=>{
     const rs = radioSelecionado()
-
+    
     if(rs!=undefined){
         //".parentNode" uma vez pega o pai de "radioSelecionado". duas vezes pega o avô.
         //".firstChild" pega o primeiro filho do avô que é o object text.
@@ -48,12 +62,11 @@ btnCursoSelecionado.addEventListener("click",()=>{
     }else{
         alert("Por favor selecione um curso.")
     }
-
 })
 
 btnRemoverCurso.addEventListener("click",()=>{
     const rs = radioSelecionado()
-
+    
     if(rs!=undefined){
         const cursoSelecionado = rs.parentNode.parentNode
     
@@ -61,5 +74,43 @@ btnRemoverCurso.addEventListener("click",()=>{
         cursoSelecionado.remove()
     }else{
         alert("Selecione um curso para poder remover.")
+    }
+})
+
+btnAdicionarAntes.addEventListener("click",()=>{
+    const valor = nomeCurso.value
+    const rs = radioSelecionado()
+    
+    if(rs!=undefined){
+        if(nomeCurso.value != ""){
+            const cursoSelecionado = rs.parentNode.parentNode
+            const novoCurso = criarNovoCurso(valor)
+    
+            //".insertBefore": adicionar antes. dentro do parenteses precisa de dois parâmetros... o que quer adicionar e antes de quem. neste caso, adicionar novoCurso antes de cursoSelecionado.
+            caixaCursos.insertBefore(novoCurso,cursoSelecionado)
+        }else{
+            alert("Digite um curso para poder ser adicionado")
+        }
+    }else{
+        alert("Selecione um curso para poder adicionar antes.")
+    }
+})
+
+btnAdicionarDepois.addEventListener("click",()=>{
+    const valor = nomeCurso.value
+    const rs = radioSelecionado()
+    
+    if(rs!=undefined){
+        if(nomeCurso.value != ""){
+            const cursoSelecionado = rs.parentNode.parentNode
+            const novoCurso = criarNovoCurso(valor)
+    
+            //não existe o insertAfter. porém, é possível adicionar antes do próximo irmão de cursoSelecionado.
+            caixaCursos.insertBefore(novoCurso,cursoSelecionado.nextSibling)
+        }else{
+            alert("Digite um curso para poder ser adicionado")
+        }
+    }else{
+        alert("Selecione um curso para poder adicionar depois.")
     }
 })
