305e151b1ddc960d21da27ee2b8e17aeb058806f
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-12-28 18:33:57 -0300

    correções
    modificado todos os nomes dos arquivos html para identificar melhor qual aula é.

--- CFBCursos/aula49/aula49.js
+++ CFBCursos/aula49/aula49.js
@@ -7,7 +7,7 @@ p_array.innerHTML = elementos_array
 
 btnVerificar.addEventListener("click",()=>{
 
-    //".some(e,i,a)" chama ou execura uma função e funciona basicamente como o .map(e,i,a), percorre um array, elemento por elemento, retornando o elemento (e), indice (i) e o proprio array (a) que satisfazer a função executada.
+    //".some(e,i,a)" chama ou executa uma função e funciona basicamente como o .map(e,i,a), percorre um array, elemento por elemento, retornando o elemento (e), indice (i) e o proprio array (a) que satisfazer a função executada.
     const ret = elementos_array.some((e,i)=>{
         if( e < 18){
             resultado.innerHTML = "Array não conforme na posição " + (i + 1)
