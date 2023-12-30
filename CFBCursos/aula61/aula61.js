3bd8fbb594c5c8e90d1e94b1d9c02b4c9bd9c78b
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2023-12-30 13:27:57 -0300

    feito exercicio aula 61

--- CFBCursos/aula61/aula61.js
+++ CFBCursos/aula61/aula61.js
@@ -1,7 +1,7 @@
 const pessoa = {
-    nome,
+    nome:"",
     
-    idade,
+    idade:"",
 
     getNome:function(){
         return this.nome
@@ -16,19 +16,40 @@ const pessoa = {
     },
 
     setIdade:function(idade){
-        this.nome = idade
+        this.idade = idade
     }
 }
 
-//todos apontam para a mesma constante e retornam a mesma coisa.
-const p2 = pessoa
-const p3 = pessoa
+// //todos apontam para a mesma constante e retornam a mesma coisa.
+// const p2 = pessoa
+// const p3 = pessoa
 
-//modifica o conteúdo/valor do atributo "nome" com o uso de sintax diferentes.
-p3.nome = "renato"
-p3["nome"] = "renato"
-pessoa.setNome("gomes")
+// //modifica o conteúdo/valor do atributo "nome" com o uso de sintax diferentes.
+// p3.nome = "renato"
+// p3["nome"] = "renato"
+// pessoa.setNome("gomes")
 
-console.log(pessoa.nome)
-console.log(p2.getNome())
-console.log(p3.nome)
+// console.log(pessoa.nome)
+// console.log(p2.getNome())
+// console.log(p3.nome)
+
+const btn_add = document.querySelector("#btn_add")
+const res = document.querySelector(".res")
+
+
+const adicionar = ()=>{
+    const f_nome = document.querySelector("#f_nome")
+    const f_idade = document.querySelector("#f_idade")
+    
+    pessoa.setNome(f_nome.value)
+    pessoa.setIdade(f_idade.value)
+
+    const div = document.createElement("div")
+    div.setAttribute("class","pessoa")
+    div.innerHTML = `Nome: ${pessoa.getNome()} <br> Idade: ${pessoa.getIdade()}`
+    res.appendChild(div)
+}
+
+btn_add.addEventListener("click",()=>{
+    adicionar()
+})
