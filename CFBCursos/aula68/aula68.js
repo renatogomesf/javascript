20dbca51d95cda455ec5faab63c93f5bc7e122b7
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-01-17 10:12:00 -0300

    feita aula 68

---
+++ CFBCursos/aula68/aula68.js
@@ -0,0 +1,67 @@
+class carro{
+    constructor(tipo,estagioTurbo){
+        this.turbo = new turbo(estagioTurbo)
+        if(tipo==1){
+            this.velmax = 120
+            this.nome = "normal"
+        }else if(tipo==2){
+            this.velmax = 160
+            this.nome = "esportivo"
+        }else if(tipo==3){
+            this.velmax = 200
+            this.nome = "super esportivo"
+        }
+
+        this.velmax += this.turbo.potencia
+    }
+
+    info(){
+        console.log(this.nome)
+        console.log(this.velmax)
+        console.log(this.turbo)
+        console.log("-----------------------")
+    }
+}
+
+class turbo{
+    constructor(e){
+        if(e==0){
+            this.potencia = 0
+        }else if(e==1){
+            this.potencia = 50
+        }else if(e==2){
+            this.potencia = 75
+        }else if(e==3){
+            this.potencia = 100
+        }
+    }
+}
+
+class carroEspecial extends carro{
+    constructor(estagioTurbo){
+        super(4,estagioTurbo)
+        this.tipoInfo = 0
+        this.velmax = 300 + this.turbo.potencia
+        this.nome = "carro especial"
+    }
+
+    //este info sobrepoe o info da classe pai. mas, para chamar o info da classe pai, basta usar o "super"
+    info(){
+        if(this.tipoInfo == 1){
+            super.info()
+        }else{
+            console.log(`nome: ${this.nome}`)
+            console.log(`vel.max: ${this.velmax}`)
+            console.log(`turbo: ${this.turbo.potencia}`)
+            console.log("-----------------------")
+        }
+    }
+}
+
+const c1 = new carro(1,0)
+const c2 = new carro(1,1)
+const c3 = new carroEspecial(3)
+
+c1.info()
+c2.info()
+c3.info()
