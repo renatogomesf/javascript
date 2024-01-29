af9a491a101142b98ecf63625366e992ead9f572
Renato Gomes <136255057+renatogomesf@users.noreply.github.com>
2024-01-29 09:35:25 -0300

    feita aula 74

---
+++ CFBCursos/aula74/aula74.js
@@ -0,0 +1,6 @@
+const menu01 = document.querySelector(".menu01")
+const link01 = document.querySelector(".link01")
+
+menu01.addEventListener("click",()=>{
+    link01.classList.toggle("link01_mostrar")
+})
